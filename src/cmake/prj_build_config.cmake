480f75e39dd05a4c4fb671d6220a5a4f458a1aa4
xelonezhwl <xelonezhwl@gmail.com>
2014-05-23 14:10:51 +0800

    调整目录

---
+++ src/cmake/prj_build_config.cmake
@@ -0,0 +1,14 @@
+
+
+set(CMAKE_BUILD_TYPE Debug)
+
+set(BUILD_examples ON)
+set(BUILD_addons ON)
+
+set(BUILD_duixengine ON)
+set(BUILD_duixengine_shared OFF)
+set(BUILD_duixengine_static ON)
+
+set(BUILD_TARGET_WIN64_PLATFORM OFF)
+set(DUIX_BUILD_INSTALLATION OFF)
+# (DUIX_BUILD_RELEASE OFF)
